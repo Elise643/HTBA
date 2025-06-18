f7c77c64365376dd739d06e7c836f22bb0d8c416
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-06-18 15:23:59 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA4CjV2R9NivDMmpnbtIA-DFwj9TwJBe4Q",
+  authDomain: "htba-b7975.firebaseapp.com",
+  projectId: "htba-b7975",
+  storageBucket: "htba-b7975.firebasestorage.app",
+  messagingSenderId: "380246220203",
+  appId: "1:380246220203:web:3ff4ad0d1d93d2d79946ee",
+  measurementId: "G-XKLZFE6P67"
+};
+
+
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
