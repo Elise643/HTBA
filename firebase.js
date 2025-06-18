a26360bd932723df1060194b860b66718450a61c
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-06-18 15:33:11 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyA4CjV2R9NivDMmpnbtIA-DFwj9TwJBe4Q",
   authDomain: "htba-b7975.firebaseapp.com",
   projectId: "htba-b7975",
-  storageBucket: "htba-b7975.firebasestorage.app",
+  storageBucket: "htba-b7975.appspot.com",
   messagingSenderId: "380246220203",
   appId: "1:380246220203:web:3ff4ad0d1d93d2d79946ee",
   measurementId: "G-XKLZFE6P67"
 };
 
-
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
+firebase.initializeApp(firebaseConfig);
+window.auth = firebase.auth(); 
