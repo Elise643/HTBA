dab5d23653ff63c72eed8a384c92ce8b52dbfd6d
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-06-30 13:04:18 -0500

    multistaff

--- staff.js
+++ staff.js
@@ -14,6 +14,19 @@ document.addEventListener("DOMContentLoaded", () => {
           staffHolder.innerHTML = "No staff found. Dangerous.";
           return;
         }
+        const nurse = document.createElement("table");
+        nurse.appendChild(document.createElement("th"));
+        nurse.firstChild.appendChild(document.createElement("td"));
+        nurse.firstChild.firstChild.textContent = "Nurse(s)";
+        const teacher = document.createElement("table");
+        teacher.appendChild(document.createElement("th"));
+        teacher.firstChild.appendChild(document.createElement("td"));
+        teacher.firstChild.firstChild.textContent = "Teacher(s)";
+        const principal = document.createElement("table");
+        principal.appendChild(document.createElement("th"));
+        principal.firstChild.appendChild(document.createElement("td"));
+        principal.firstChild.firstChild.textContent = "Principal";
+
         snapshot.forEach(doc => {
           const userData = doc.data();
           if (userData && userData.type === "staff"){
@@ -22,19 +35,40 @@ const staffName =
   (userData.firstName ? userData.firstName.trim() + " " : "") +
   (userData.lastName || "");
 
-         const divS = document.createElement("div"); 
+         const person = document.createElement("td"); 
           const img = document.createElement("img");
           img.classList.add("bigPFP");
           img.src = userData.photoURL || "images/defaultPFP.png";
           img.alt = staffName || "Profile image";
-          staffHolder.appendChild(divS);
-          divS.innerHTML = `${staffName}: <br>`;
-          divS.appendChild(img);
+          if (userData.role === "nurse") {
+              if (!nurse.querySelector("tr")){
+                nurse.appendChild(document.createElement("tr"));
+              }
+              nurse.querySelector("tr").appendChild(person);
+          }
+          if (userData.role === "teacher") {
+              if (!teacher.querySelector("tr")){
+                teacher.appendChild(document.createElement("tr"));
+              }
+              teacher.querySelector("tr").appendChild(person);
+          }
+          if (userData.role === "principal") {
+              if (!principal.querySelector("tr")){
+                principal.appendChild(document.createElement("tr"));
+              }
+              princial.querySelector("tr").appendChild(person);
+          }
+          person.innerHTML = `${staffName}: <br>`;
+          person.appendChild(img);
           }
         });
+        if (principal.querySelector("tr")) staffHolder.appendChild(principal);
+        if (teacher.querySelector("tr")) staffHolder.appendChild(teacher);
+        if (nurse.querySelector("tr")) staffHolder.appendChild(nurse);
+
 
       } catch (error) {
-        staffHolder.innerHTML = `<tr><td colspan="3">Error: ${error.message}</td></tr>`;
+        staffHolder.innerHTML = `Error: ${error.message}`;
       }
     
     });
