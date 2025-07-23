6e1068a20552bfb8041319d0ebf7ce0b6a145a03
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-07-23 16:14:16 -0500

    tasks

---
+++ tasks.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("firebase-ready", () => {
+        const container = document.querySelecter("#tasks-container");
+        auth.onAuthStateChanged(async user => {
+    if (!user) {
+        container.innerHTML = "<p>What are you even doing? You're not logged in. Stop looking for homework and just log in to your own account, E.N.</p>"
+    } else {
+        try {
+            const userDoc = await db.collection("users").doc(user.uid).get();
+            const userData = userDoc.data();
+            if (userData.taskList && userData.taskList.length>0) {
+                container.innerHTML = userData.taskList;
+            }
+            else {
+                        container.innerHTML = "<p>Congratulations, you're task-free! That could mean you don't have any tasks, or just that they haven't been added here.</p>"
+
+            }
+        } catch (error) {
+            console.error("Failed to load user data:", error);
+        }
+    }
+});
+
+    });
+});
