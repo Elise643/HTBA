ddeb02c6b95a1eefad994ef827c14a9b3da6943f
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-07-25 18:57:17 -0500

    fully block

--- readBlocker.js
+++ readBlocker.js
@@ -4,3 +4,4 @@ overlay.innerHTML = `
 <div class="new-task-menu"><p style="margin:auto; font-size:3em;">LEAVE<br>THIS IS NOT FUNCTIONAL</p><button type="button" onclick="javascript:history.back()">Back</button></div>
 `
 document.body.appendChild(overlay);
+document.body.style = "overflow-y:hidden; pointer-events:none; touch-action:none;";
