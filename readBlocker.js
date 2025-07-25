c95525e381d8fe7563f14bcdc3efe89cd287fd73
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-07-25 18:43:44 -0500

    fix readblocker

--- readBlocker.js
+++ readBlocker.js
@@ -1,5 +1,6 @@
 const overlay = document.createElement("div");
 overlay.classList.add("overlay");
 overlay.innerHTML = `
-<div class="new-task-menu">LEAVE<br>THIS IS NOT FUNCTIONAL</div>
+<div class="new-task-menu"><p style="margin:auto">LEAVE<br>THIS IS NOT FUNCTIONAL</p></div>
 `
+document.body.appendChild(overlay);
