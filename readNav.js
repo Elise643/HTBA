aaf197ca3a7c3d352c53c82ffb98bca4070d17d1
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-07-26 16:17:42 -0500

    fix parentage

--- readNav.js
+++ readNav.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggles = document.querySelectorAll(".read-tree-menu .toggle");
   toggles.forEach(btn => {
     btn.addEventListener("click", () => {
-      const parent = btn.parentElement;
+      const parent = btn.parentElement.parentElement;
       parent.classList.toggle("open");
       btn.querySelector("img").style.transform.toggle("rotate(180deg)");
     });
