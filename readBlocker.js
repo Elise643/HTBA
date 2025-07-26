4b2dad8276789d10f09317e492597339d3072e6f
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-07-25 19:12:53 -0500

    hate the user

--- readBlocker.js
+++ readBlocker.js
@@ -1,7 +1,7 @@
 const overlay = document.createElement("div");
 overlay.classList.add("dimmingOverlay");
 overlay.innerHTML = `
-<div class="new-task-menu"><p style="margin:auto; font-size:3em;">LEAVE<br>THIS IS NOT FUNCTIONAL</p><button type="button" onclick="javascript:history.back()">Back</button></div>
+<div class="block-read"><p>LEAVE.<br>THIS IS NOT FUNCTIONAL</p><button type="button" class="back-block-button" onclick="javascript:history.back()">Back</button></div>
 `
 document.body.appendChild(overlay);
 document.body.style = "overflow-y:hidden; pointer-events:none; touch-action:none;";
