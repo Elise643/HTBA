3b5e643c24155e5180913184506400fa56ad557d
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-08-22 08:02:23 -0500

    Add loading and censoring better

--- readCensorship.js
+++ readCensorship.js
@@ -3,13 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
         auth.onAuthStateChanged(async user => {
             if (!user) {
                 document.querySelector("#read-doc-holder").innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
+                return;
             } else {
                 const userDoc = await db.collection("users").doc(user.uid).get();
                 const userData = userDoc.data();
                 if (userData.type === "visitor" || userData.type === "Visitor") {
                 document.querySelector("#read-doc-holder").innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
-                }
+                return;
             }
+            }
+
+            document.querySelector("read-doc-holder").style.display = "block";
+            document.querySelector("#loadMessage").remove;
         });
     });
 });
