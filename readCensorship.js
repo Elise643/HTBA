ef343a61d1d22cc825f72eb546a8bb4dea316b4f
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-08-22 07:56:51 -0500

    fix mistake of me being an idiot who doesn't know how to write basic code

--- readCensorship.js
+++ readCensorship.js
@@ -2,13 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("firebase-ready", () => {
         auth.onAuthStateChanged(async user => {
             if (!user) {
-                document.querySelector.innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
+                document.querySelector("#read-doc-holder").innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
             } else {
                 const userDoc = await db.collection("users").doc(user.uid).get();
                 const userData = userDoc.data();
                 if (userData.type === "visitor" || userData.type === "Visitor") {
-                document.querySelector.innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
-
+                document.querySelector("#read-doc-holder").innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
                 }
             }
         });
