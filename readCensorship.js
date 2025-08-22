9b39dd6df7cc1af52ea4e1f2724e803f0d6fc100
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-08-22 07:54:05 -0500

    censorship but more efficient hopepfullu

--- readCensorship.js
+++ readCensorship.js
@@ -2,16 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("firebase-ready", () => {
         auth.onAuthStateChanged(async user => {
             if (!user) {
-                document.querySelectorAll("iframe").forEach(el => {
-                    el.remove();
-                });
+                document.querySelector.innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
             } else {
                 const userDoc = await db.collection("users").doc(user.uid).get();
                 const userData = userDoc.data();
                 if (userData.type === "visitor" || userData.type === "Visitor") {
-                    document.querySelectorAll("iframe").forEach(el => {
-                        el.remove();
-                    });
+                document.querySelector.innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
+
                 }
             }
         });
