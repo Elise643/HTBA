2ca47a36e4f4d6594b12774e535ead44ce2c7860
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-08-22 08:10:18 -0500

    things should be fixed now?

--- readCensorship.js
+++ readCensorship.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("firebase-ready", () => {
         auth.onAuthStateChanged(async user => {
             if (!user) {
+                                document.querySelector("#loadMessage").remove;
+
                 document.querySelector("#read-doc-holder").innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
-                            document.querySelector("#read-doc-holder").style.display = "block";
-                                        document.querySelector("#loadMessage").remove;
+                document.querySelector("#read-doc-holder").style.display = "block";
 
 
                 return;
@@ -12,16 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
                 const userDoc = await db.collection("users").doc(user.uid).get();
                 const userData = userDoc.data();
                 if (userData.type === "visitor" || userData.type === "Visitor") {
+                                                            document.querySelector("#loadMessage").remove;
+
                 document.querySelector("#read-doc-holder").innerHTML = "Oops! You don't seem to be signed in. (Or you're signed in to an account that doesn't have script reading access)";
                 
                             document.querySelector("#read-doc-holder").style.display = "block";
-                                        document.querySelector("#loadMessage").remove;
 
 return;
             }
             }
 
-            document.querySelector("read-doc-holder").style.display = "block";
+            document.querySelector("#read-doc-holder").style.display = "block";
             document.querySelector("#loadMessage").remove;
         });
     });
