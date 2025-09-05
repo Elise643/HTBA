2a87396adad0e466cb3114c22151b23f89f0e067
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-09-05 10:17:02 -0500

    Ship functionality

--- ships.js
+++ ships.js
@@ -22,4 +22,22 @@ let val1 = document.querySelector("#shipSelectOne").value;
 let val2 = document.querySelector("#shipSelectTwo").value;
 let p = document.querySelector("#shipResult");
 p.innerHTML = `Value 1 is ${val1}<br>Value 2 is ${val2}`;
+let ship = null;
+let s = [val1,val2];
+s.sort();
+let searchValue = s.join(":");
+fetch('ships.json')
+  .then(response => response.json())
+  .then(data => {
+    const item = data.find(obj => obj["Pair"] === searchValue);
+    ship = item ? item["Ship"] : null;
+  })
+  if (!ship === null) {
+    p.innerHTML += `<br><br>Ship name is ${ship}`;
+  }
+  else {
+    p.innerHTML += `<br><br>No ship name found in data`;
+  }
+
+
 }
