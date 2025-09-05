e93caa2f785dc795fa504ad1cba5cf4e3850916b
Elise643 <83870589+Elise643@users.noreply.github.com>
2025-09-05 10:26:33 -0500

    fix accidentally deleting important stuff

--- ships.js
+++ ships.js
@@ -1,3 +1,22 @@
+let scripties = ['Alvin','Azari','Circe','Clara','Clyde','Con','DevOn','E.N.','Ephorto','Guyekio','Kaya','Kira','Opie','Ryder','Serina','Seth','Tyler','Virha','Winter'];
+for (let char of scripties) {
+    let op1 = document.createElement("option");
+    let op2 = document.createElement("option");
+    op1.textContent=char;
+    op2.textContent=char;
+    op1.value=char;
+    op2.value=char;
+    document.querySelector("#shipSelectOne").appendChild(op1);
+    document.querySelector("#shipSelectTwo").appendChild(op2);
+}
+
+document.querySelector("#shipSelectOne").addEventListener("change",function () {
+    loadShip();
+});
+document.querySelector("#shipSelectTwo").addEventListener("change", function() {
+    loadShip();
+});
+
 function loadShip() {
   let val1 = document.querySelector("#shipSelectOne").value;
   let val2 = document.querySelector("#shipSelectTwo").value;
