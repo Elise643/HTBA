bd9fa7ee251def971eb5550c3facb5d6e5dff06f
Elise643 <83870589+Elise643@users.noreply.github.com>
2026-09-11 18:09:18 -0500

    fix the thing

--- locationswap.js
+++ locationswap.js
@@ -4,19 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
         const db = firebase.firestore();
 
         auth.onAuthStateChanged((user) => {
-            
+            console.log(document.cookie.includes("atschoollocation=arkansas") ? "arkansas":"wyoming")
             const existingTog = document.getElementById("toggle-wrap");
             if (existingTog) existingTog.remove();
 
             let toggle = false;
             let wy = document.cookie.includes("atschoollocation=arkansas");
+            
             if (user) {
                 db.collection("users").doc(user.uid).get()
                     .then((doc) => {
-                        console.log(doc.role)
-                        if (doc.role === "admin" || doc.location.length > 1) toggle++;
+                        const userData = doc.exists ? doc.data() : {};
+                        if (userData.role === "admin" || userData.location.length > 1) toggle++;
                         else {
-                            document.cookie = `atschoollocation=${doc.location[0].replace("AR", "arkansas").replace("WY", "wyoming")}`
+                            document.cookie = `atschoollocation=${userData.location[0].replace("AR", "arkansas").replace("WY", "wyoming")}`
                         }
                     });
             }
@@ -27,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 togDiv.classList.add("toggle-wrap");
                 togDiv.id = "toggle-wrap"
                 togDiv.innerHTML = `
-                    <input type="checkbox" id="stateToggle" class="toggle-input">
+                    <input type="checkbox" ${wy ? "checked":""}id="stateToggle" class="toggle-input">
                     <label for="stateToggle" class="toggle-label">
                         <span class="toggle-text left">WY</span>
                         <span class="toggle-text right">AR</span>
@@ -36,6 +37,11 @@ document.addEventListener("DOMContentLoaded", () => {
                     </label>
                 `
                 document.querySelector("header").appendChild(togDiv)
+                document.querySelector("#stateToggle").addEventListener("change",(event)=>{
+                    
+                        document.cookie = `atschoollocation=${event.target.checked ? "wyoming":"arkansas"}`;
+                        console.log(document.cookie.includes("atschoollocation=arkansas") ? "arkansas":"wyoming")
+                })
 
             }
 
