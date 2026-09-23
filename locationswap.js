885d0f39081470c768b88778cfeab8f1d8d7409f
Elise643 <83870589+Elise643@users.noreply.github.com>
2026-09-22 19:21:11 -0500

    actually update the page

--- locationswap.js
+++ locationswap.js
@@ -58,13 +58,15 @@ document.addEventListener("DOMContentLoaded", () => {
                             togDiv.querySelector("#stateToggle").addEventListener("change", (event) => {
 
                                 document.cookie = `schoollocation=${document.getElementById("stateToggle").checked ? "WY" : "AR"}`;
-                                console.log(document.cookie)
+                                console.log(document.cookie);
+                                updatePage();
                             })
                             document.querySelector("header").appendChild(togDiv)
 
                         
 
             }
+            updatePage()
         });
     });
 });
@@ -76,51 +78,12 @@ function getCookieByName(name) {
     }
     return null;
 }
-/*                      
-        auth.onAuthStateChanged((user) => {
-            console.log(document.cookie.includes("atschoollocation=arkansas") ? "arkansas":"wyoming")
-            const existingTog = document.getElementById("toggle-wrap");
-            if (existingTog) existingTog.remove();
 
-            let toggle = false;
-            
-            
-            if (user) {
-                db.collection("users").doc(user.uid).get()
-                    .then((doc) => {
-                        const userData = doc.exists ? doc.data() : {};
-                        if (userData.role === "admin" || userData.location.length > 1) toggle++;
-                        else {
-                            document.cookie = `atschoollocation=${userData.location[0].replace("AR", "arkansas").replace("WY", "wyoming")}`
-                        }
-                    });
-            }
-            else toggle++;
-            let wy = document.cookie.includes("atschoollocation=arkansas");
-            console.log(document.cookie.includes("atschoollocation=arkansas") ? "arkansas":"wyoming")
-            if (toggle) {
-                let togDiv = document.createElement("div");
-                togDiv.classList.add("toggle-wrap");
-                togDiv.id = "toggle-wrap"
-                togDiv.innerHTML = `
-                    <input type="checkbox" ${wy ? "":"checked"}id="stateToggle" class="toggle-input">
-                    <label for="stateToggle" class="toggle-label">
-                        <span class="toggle-text left">WY</span>
-                        <span class="toggle-text right">AR</span>
-                        <span class="toggle-knob"></span>
-                        <span class="toggle-background"></span>
-                    </label>
-                `
-                document.querySelector("header").appendChild(togDiv)
-                document.querySelector("#stateToggle").addEventListener("change",(event)=>{
-                    
-                        document.cookie = `atschoollocation=${!event.target.checked ? "wyoming":"arkansas"}`;
-                        console.log(document.cookie.includes("atschoollocation=arkansas") ? "arkansas":"wyoming")
-                })
-
-            }
-
-        });
-    });
-});
-*/
+function updatePage(){
+    let location = getCookieByName("schoollocation");
+    let hI = document.getElementById("headerIcon");
+    hI.src = `images/${location==="WY"?"AT":"IN"}-School-Logo.png`
+    hI.alt = `${location==="WY"?"AT":"IN"} School Logo`
+    let footerText = document.querySelector("footer");
+    footerText.innerHTML =   `&copy; 2025-2026 ${location==="WY"?"AT":"IN"} School`
+}
