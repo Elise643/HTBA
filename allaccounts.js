5127a56210318a724821c2aeaba4573cd6761187
Elise643 <83870589+Elise643@users.noreply.github.com>
2026-09-24 14:02:11 -0500

    maybe THIS will work

--- allaccounts.js
+++ allaccounts.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
               <p class="displayName">${fullName}</p>
               <p class="username">@${userData.displayName}</p>
               <p class="pronouns">${userData.pronouns || "No pronouns found."}</p>
-              <p class="username">${(userData.role ? userData.role[currentLoc] || userData.role || userData.type:userData.type).charAt(0).toUpperCase() + (userData.role[currentLoc] || userData.role || userData.type).slice(1)}</p>
+              <p class="username">${(userData.role ? userData.role[currentLoc] || userData.role || userData.type:userData.type).charAt(0).toUpperCase() + (userData.role ? userData.role[currentLoc] || userData.role || userData.type:userData.type).slice(1)}</p>
               </a>
               </div>
             `;
